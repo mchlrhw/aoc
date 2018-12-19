d79b8a426041c9f0789aedaed61c806bbf9c24fa
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2018-12-19 09:25:38 +0000

    Solve year 2018 day 10 parts 1 and 2

---
+++ python/aoc/year_2018/day_10/__init__.py
@@ -0,0 +1,80 @@
+import re
+from dataclasses import dataclass
+from operator import attrgetter
+from typing import Iterable, List, Tuple
+
+
+POINT_REGEX = re.compile(r'position=<(?P<x>[ ]*-?[0-9]+),(?P<y>[ ]*-?[0-9]+)> velocity=<(?P<vx>[ ]*-?[0-9]+),(?P<vy>[ ]*-?[0-9]+)>')
+
+
+class InvalidPoint(Exception):
+    pass
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+    vx: int = 0
+    vy: int = 0
+
+
+def parse_point(point_spec: str) -> Point:
+    m = POINT_REGEX.match(point_spec)
+    if m is None:
+        raise InvalidPoint()
+
+    try:
+        x = int(m.group('x'))
+        y = int(m.group('y'))
+        vx = int(m.group('vx'))
+        vy = int(m.group('vy'))
+    except ValueError:
+        raise InvalidPoint()
+
+    return Point(x, y, vx, vy)
+
+
+def get_limits(points: Iterable[Point]) -> Tuple[Point, Point]:
+    min_x = min(points, key=attrgetter('x')).x
+    min_y = min(points, key=attrgetter('y')).y
+    origin = Point(min_x, min_y)
+
+    max_x = max(points, key=attrgetter('x')).x
+    max_y = max(points, key=attrgetter('y')).y
+    limit = Point(max_x, max_y)
+
+    return origin, limit
+
+
+def normalise_point(point: Point, origin: Point) -> Point:
+    return Point(point.x - origin.x, point.y - origin.y)
+
+
+def plot_points(points: Iterable[Point]) -> str:
+    origin, limit = get_limits(points)
+    normalised = [normalise_point(p, origin) for p in points]
+
+    limit = normalise_point(limit, origin)
+    origin = normalise_point(origin, origin)
+
+    width, height = limit.x + 1, limit.y + 1
+    plane = []
+    for _ in range(height):
+        row = []
+        for _ in range(width):
+            row.append('.')
+        plane.append(row)
+
+    for p in normalised:
+        plane[p.y][p.x] = '#'
+
+    output = ''
+    for row in plane:
+        output += ''.join(row) + '\n'
+
+    return output
+
+
+def move_points(points: Iterable[Point]) -> List[Point]:
+    return [Point(p.x + p.vx, p.y + p.vy, p.vx, p.vy) for p in points]
---
+++ python/tests/year_2018/test_day_10.py
@@ -0,0 +1,202 @@
+import pytest
+
+from aoc.year_2018.day_10 import get_limits, move_points
+from aoc.year_2018.day_10 import parse_point, plot_points
+from aoc.year_2018.day_10 import Point
+from aoc.year_2018.day_10.data import puzzle_input
+
+
+@pytest.mark.parametrize(
+    'point_spec, expected',
+    [
+        ('position=< 9,  1> velocity=< 0,  2>', Point(9, 1, 0, 2)),
+        ('position=< 7,  0> velocity=<-1,  0>', Point(7, 0, -1, 0)),
+        ('position=< 3, -2> velocity=<-1,  1>', Point(3, -2, -1, 1)),
+        ('position=< 6, 10> velocity=<-2, -1>', Point(6, 10, -2, -1)),
+        ('position=< 2, -4> velocity=< 2,  2>', Point(2, -4, 2, 2)),
+        ('position=<-6, 10> velocity=< 2, -2>', Point(-6, 10, 2, -2)),
+        ('position=< 1,  8> velocity=< 1, -1>', Point(1, 8, 1, -1)),
+        ('position=< 1,  7> velocity=< 1,  0>', Point(1, 7, 1, 0)),
+        ('position=<-3, 11> velocity=< 1, -2>', Point(-3, 11, 1, -2)),
+        ('position=< 7,  6> velocity=<-1, -1>', Point(7, 6, -1, -1)),
+        ('position=<-2,  3> velocity=< 1,  0>', Point(-2, 3, 1, 0)),
+        ('position=<-4,  3> velocity=< 2,  0>', Point(-4, 3, 2, 0)),
+        ('position=<10, -3> velocity=<-1,  1>', Point(10, -3, -1, 1)),
+        ('position=< 5, 11> velocity=< 1, -2>', Point(5, 11, 1, -2)),
+        ('position=< 4,  7> velocity=< 0, -1>', Point(4, 7, 0, -1)),
+        ('position=< 8, -2> velocity=< 0,  1>', Point(8, -2, 0, 1)),
+        ('position=<15,  0> velocity=<-2,  0>', Point(15, 0, -2, 0)),
+        ('position=< 1,  6> velocity=< 1,  0>', Point(1, 6, 1, 0)),
+        ('position=< 8,  9> velocity=< 0, -1>', Point(8, 9, 0, -1)),
+        ('position=< 3,  3> velocity=<-1,  1>', Point(3, 3, -1, 1)),
+        ('position=< 0,  5> velocity=< 0, -1>', Point(0, 5, 0, -1)),
+        ('position=<-2,  2> velocity=< 2,  0>', Point(-2, 2, 2, 0)),
+        ('position=< 5, -2> velocity=< 1,  2>', Point(5, -2, 1, 2)),
+        ('position=< 1,  4> velocity=< 2,  1>', Point(1, 4, 2, 1)),
+        ('position=<-2,  7> velocity=< 2, -2>', Point(-2, 7, 2, -2)),
+        ('position=< 3,  6> velocity=<-1, -1>', Point(3, 6, -1, -1)),
+        ('position=< 5,  0> velocity=< 1,  0>', Point(5, 0, 1, 0)),
+        ('position=<-6,  0> velocity=< 2,  0>', Point(-6, 0, 2, 0)),
+        ('position=< 5,  9> velocity=< 1, -2>', Point(5, 9, 1, -2)),
+        ('position=<14,  7> velocity=<-2,  0>', Point(14, 7, -2, 0)),
+        ('position=<-3,  6> velocity=< 2, -1>', Point(-3, 6, 2, -1)),
+    ],
+)
+def test_parse_point_examples(point_spec, expected):
+    point = parse_point(point_spec)
+
+    assert point == expected
+
+
+test_input = """
+position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>
+"""
+
+
+def test_get_limits_example():
+    points = [parse_point(p) for p in test_input.splitlines() if p.strip()]
+    expected = (Point(-6, -4), Point(15, 11))
+
+    result = get_limits(points)
+
+    assert result == expected
+
+
+def test_plot_points_example():
+    points = [parse_point(p) for p in test_input.splitlines() if p.strip()]
+    expected = """\
+........#.............
+................#.....
+.........#.#..#.......
+......................
+#..........#.#.......#
+...............#......
+....#.................
+..#.#....#............
+.......#..............
+......#...............
+...#...#.#...#........
+....#..#..#.........#.
+.......#..............
+...........#..#.......
+#...........#.........
+...#.......#..........
+"""
+
+    output = plot_points(points)
+
+    assert output == expected
+
+
+def test_move_points():
+    points = [parse_point(p) for p in test_input.splitlines() if p.strip()]
+    examples = [
+"""\
+........#....#....
+......#.....#.....
+#.........#......#
+..................
+....#.............
+..##.........#....
+....#.#...........
+...##.##..#.......
+......#.#.........
+......#...#.....#.
+#...........#.....
+..#.....#.#.......
+""",
+"""\
+..........#...
+#..#...####..#
+..............
+....#....#....
+..#.#.........
+...#...#......
+...#..#..#.#..
+#....#.#......
+.#...#...##.#.
+....#.........
+""",
+"""\
+#...#..###
+#...#...#.
+#...#...#.
+#####...#.
+#...#...#.
+#...#...#.
+#...#...#.
+#...#..###
+""",
+"""\
+........#....
+....##...#.#.
+..#.....#..#.
+.#..##.##.#..
+...##.#....#.
+.......#....#
+..........#..
+#......#...#.
+.#.....##....
+...........#.
+...........#.
+"""
+    ]
+
+    for expected in examples:
+        points = move_points(points)
+        output = plot_points(points)
+
+        assert output == expected
+
+
+def test_move_points_puzzle_input():
+    expected = """\
+######..#....#....##....#.......#.......#....#..#.......#####.
+#.......#...#....#..#...#.......#.......#...#...#.......#....#
+#.......#..#....#....#..#.......#.......#..#....#.......#....#
+#.......#.#.....#....#..#.......#.......#.#.....#.......#....#
+#####...##......#....#..#.......#.......##......#.......#####.
+#.......##......######..#.......#.......##......#.......#....#
+#.......#.#.....#....#..#.......#.......#.#.....#.......#....#
+#.......#..#....#....#..#.......#.......#..#....#.......#....#
+#.......#...#...#....#..#.......#.......#...#...#.......#....#
+######..#....#..#....#..######..######..#....#..######..#####.
+"""
+
+    points = [parse_point(p) for p in puzzle_input.splitlines() if p.strip()]
+    for i in range(1, 10228):
+        points = move_points(points)
+    output = plot_points(points)
+
+    assert output == expected  # part 1
+    assert i == 10227          # part 2

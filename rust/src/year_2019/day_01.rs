b67d2dcae49fc85935ad969fad5b1b28032301ee
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2019-12-01 14:02:58 +0000

    Solve year 2019 day 01 part 2 (Rust)

--- rust/src/year_2019/day_01.rs
+++ rust/src/year_2019/day_01.rs
@@ -1,5 +1,15 @@
 pub fn calculate_fuel(mass: u32) -> u32 {
-    (mass / 3) - 2
+    (mass / 3).checked_sub(2).unwrap_or(0)
+}
+
+pub fn calculate_fuel_inclusive(mass: u32) -> u32 {
+    let mut fuel = calculate_fuel(mass);
+
+    if fuel > 0 {
+        fuel += calculate_fuel_inclusive(fuel);
+    }
+
+    fuel
 }
 
 #[cfg(test)]
@@ -21,7 +31,7 @@ mod tests {
     }
 
     #[test]
-    fn solution() -> Result<(), std::num::ParseIntError> {
+    fn part_1_solution() -> Result<(), std::num::ParseIntError> {
         let puzzle_input = String::from_utf8_lossy(include_bytes!("day_01/input.txt"));
 
         let mut total = 0;
@@ -32,6 +42,34 @@ mod tests {
         }
 
         assert_eq!(total, 3317100);
+
+        Ok(())
+    }
+
+    #[rstest_parametrize(
+        mass,
+        expected,
+        case(12, 2),
+        case(14, 2),
+        case(1969, 966),
+        case(100756, 50346)
+    )]
+    fn fuel_calculation_inclusive_examples(mass: u32, expected: u32) {
+        assert_eq!(calculate_fuel_inclusive(mass), expected)
+    }
+
+    #[test]
+    fn part_2_solution() -> Result<(), std::num::ParseIntError> {
+        let puzzle_input = String::from_utf8_lossy(include_bytes!("day_01/input.txt"));
+
+        let mut total = 0;
+        for line in puzzle_input.lines() {
+            let mass = line.parse::<u32>()?;
+            let fuel = calculate_fuel_inclusive(mass);
+            total += fuel;
+        }
+
+        assert_eq!(total, 4972784);
 
         Ok(())
     }
